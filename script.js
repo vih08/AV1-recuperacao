0864705d22bb7261e1aeacc46f8b153892312809
VITÓRIA MARINI DA SILVA <vitoria_silva18@estudante.sesisenai.org.br>
2025-04-24 09:47:27 -0300

    Encapsulamento de propriedades sensíveis com getters e setters

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 const canvas = document.getElementById('recuperacao');
-const ctx = canvas.getContext('av1');
+const ctx = canvas.getContext('2d');
 
 class Personagem {
-constructor(força, mana, vida) {
-this.força = força;
-this.mana = mana;
-this.vida = vida;
+#forca
+#mana
+#vida
+constructor(forca, mana, vida) {
+this.#forca = forca
+this.#mana = mana
+this.#vida = vida
+}
+
+getForca() {
+    return this.#forca
+}
+
+getMana() {
+    return this.#mana
+}
+
+getVida() {
+    return this.#vida
+}
+
+setForca(forca) {
+    this.#forca = forca
+}
+
+setMana(mana) {
+    this.#mana = mana
+}
+
+setVida(vida) {
+    this.#vida = vida
 }
 
 atacar() {
@@ -18,39 +45,68 @@ defende() {
 }
 
 class Guerreiro extends Personagem {
+#arma   
     constructor(vida, mana, arma) {
         super(vida, mana)
-        this.arma = arma
+        this.#arma = arma
     }
+
+    getArma() {
+        return this.#arma
+    }
+
+    setArma(arma) {
+        this.#arma = arma
+    }
+
     atacar() {
         console.log(`${this.nome} ataca com ${this.arma}!`)
     }
+
 }
 
 class Mago extends Personagem {
+#magia    
     constructor(vida, mana, magia) {
         super(vida, mana)
-        this.magia = magia
+        this.#magia = magia
     }
+
+    getMagia() {
+        return this.#magia
+    }
+
+    setMagia(magia) {
+        this.#magia = magia
+    }
+
     usarMagia() {
         console.log(`${this.nome} usarMagia`)
     }
 }
 
 class Arqueiro extends Personagem {
+#arco
     constructor(nome, vida, mana, arco) {
         super(nome, vida, mana)
-        this.arco = arco
+        this.#arco = arco
+    }
+   
+    getArco() {
+        return this.#arco
     }
 
+    setArco(arco) {
+        this.#arco = arco
+    }
 
-atacar() {
+    atacar() {
     console.log(`${this.nome} atira com ${this.arco}!`)
     }
 }
 
-const guerreiro = new Guerreiro ("x", 10, 5, "Espada")
-const mago = new Mago("y", 10, 5, "Fogo")
+const guerreiro = new Guerreiro ("x", 10, 5, "espada")
+const mago = new Mago("y", 10, 5, "fogo")
 const arqueiro = new arqueiro('z',10, 5, 'varios arcos junto')
 
 guerreiro.atacar()
