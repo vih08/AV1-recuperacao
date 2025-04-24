18ce88a1792e8d02a996265c9fdf74762d42c2c6
VITÓRIA MARINI DA SILVA <vitoria_silva18@estudante.sesisenai.org.br>
2025-04-24 08:37:23 -0300

    Criação das classes Guerreiro, Mago e Arqueiro herdando de Personagem

--- script.js
+++ script.js
@@ -6,11 +6,37 @@ constructor(força, mana, vida) {
 this.força = força;
 this.mana = mana;
 this.vida = vida;
-}}
+}
 
-class Jogador extends Personagem {
-    constructor() {
-        super(15, 20, 5)
-    }}
-    
+atacar() {
+    console.log(`${this.nome} ataca!`)
+}
+
+defende() {
+    console.log(`${this.nome} defende!`)
+}
+}
+   
+class Guerreiro extends Personagem {
+    constructor(vida, mana, arma) {
+        super(vida, mana)
+        this.arma = arma
+    }
+}
+
+class Mago extends Personagem {
+    constructor(vida, mana, magia) {
+        super(vida, mana)
+        this.magia = magia
+    }
+}
+
+class Arqueiro extends Personagem {
+    constructor(nome, vida, mana, arco) {
+        super(nome, vida, mana)
+        this.arco = arco
+    }
+}
+
+
 
